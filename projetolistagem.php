ce64267cb8523df86f30a623b2b0b42e6feb003b
SteFagundes <137810488+SteFagundes@users.noreply.github.com>
2023-06-26 12:10:36 -0400

    projetolistagem.php

--- projetolistagem.php
+++ projetolistagem.php
@@ -1,1 +1,13 @@
+<?php
 
+$server_host = "db-ads.c8bqy6anulng.sa-east-1.rds.amazonaws.com";
+$server_user = "admin";
+$server_password = " ";
+$server_database = "base_ste";
+
+$conn = new mysqli($server_host, $server_user, $server_password, $server_database);
+
+if ($conn->connect_error) {
+  die("Falha na conexão: " . $conn->connect_error);
+}
+?>
