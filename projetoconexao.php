c28580a14a0e5a785f26bec0b66c9d91fb588588
SteFagundes <137810488+SteFagundes@users.noreply.github.com>
2023-06-26 21:04:21 -0300

    projetoconexao.php

--- projetoconexao.php
+++ projetoconexao.php
@@ -1,9 +1,9 @@
 <?php
 
-$server_host = "db-ads.c8bqy6anulng.sa-east-1.rds.amazonaws.com";
+$server_host = "localhost/3360";
 $server_user = "admin";
-$server_password = "Unimar-ads-2023";
-$server_database = "minha_base";
+$server_password = "123456";
+$server_database = "base_ste";
 
 $conn = new mysqli($server_host, $server_user, $server_password, $server_database);
 
